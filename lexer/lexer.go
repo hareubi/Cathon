3e86342ee2d69ec3e01a07b4bdbb14ce6b46efa3
indienewbies@gmail.com <indienewbies@gmail.com>
2024-09-17 21:53:08 +0900

    added int and identifier support to lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,9 @@
 package lexer
 
-import "cathon/token"
+import (
+	"cathon/token"
+	"unicode"
+)
 
 type Lexer struct {
 	input        string
@@ -32,27 +35,63 @@ func NewToken(tokenType token.TokenType, ch rune) token.Token {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.SkipWhiteSpace()
+
 	switch l.ch {
 	case '=':
 		tok = NewToken(token.TokenType(token.ASSIGN), '=')
 	case ';':
-		tok = NewToken(token.TokenType(token.ASSIGN), ';')
+		tok = NewToken(token.TokenType(token.SEMICOLON), ';')
 	case '(':
-		tok = NewToken(token.TokenType(token.ASSIGN), '(')
+		tok = NewToken(token.TokenType(token.LPAREN), '(')
 	case ')':
-		tok = NewToken(token.TokenType(token.ASSIGN), ')')
+		tok = NewToken(token.TokenType(token.RPAREN), ')')
 	case ',':
-		tok = NewToken(token.TokenType(token.ASSIGN), ',')
+		tok = NewToken(token.TokenType(token.COMMA), ',')
 	case '+':
-		tok = NewToken(token.TokenType(token.ASSIGN), '+')
+		tok = NewToken(token.TokenType(token.PLUS), '+')
 	case '{':
-		tok = NewToken(token.TokenType(token.ASSIGN), '{')
+		tok = NewToken(token.TokenType(token.LBRACE), '{')
 	case '}':
-		tok = NewToken(token.TokenType(token.ASSIGN), '}')
+		tok = NewToken(token.TokenType(token.RBRACE), '}')
 	case 0:
 		tok.Type = token.EOF
 		tok.Literal = ""
+	default:
+		if unicode.IsLetter(l.ch) {
+			tok.Literal = l.ReadIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if unicode.IsDigit(l.ch) {
+			tok.Literal = l.ReadNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = NewToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.ReadChar()
 	return tok
+}
+
+func (l *Lexer) ReadIdentifier() string {
+	position := l.position
+	for unicode.IsLetter(l.ch) {
+		l.ReadChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) ReadNumber() string {
+	position := l.position
+	for unicode.IsDigit(l.ch) {
+		l.ReadChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) SkipWhiteSpace() {
+	for unicode.IsSpace(l.ch) {
+		l.ReadChar()
+	}
 }

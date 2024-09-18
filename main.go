e759f584c85c725f35b6fa87d6a6014f8e0e796d
hareubi <70735563+hareubi@users.noreply.github.com>
2024-09-18 15:53:42 +0900

    Implemented repl

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"cathon/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the cathon programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}

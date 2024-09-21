cb92202f9a8ce1fabe988b246c7da258da6def6e
hareubi <70735563+hareubi@users.noreply.github.com>
2024-09-21 19:07:24 +0900

    Implemented Let parser and ast

---
+++ parser/parser.go
@@ -0,0 +1,75 @@
+package parser
+
+import (
+	"cathon/ast"
+	"cathon/lexer"
+	"cathon/token"
+)
+
+func New(lexerP *lexer.Lexer) *Parser {
+	p := Parser{l: lexerP}
+	return &p
+}
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func (p *Parser) NextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (parserP *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for parserP.curToken.Type != token.EOF {
+		stmt := parserP.ParseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		parserP.NextToken()
+	}
+	return program
+}
+
+func (parserP *Parser) ParseStatement() ast.Statement {
+	switch parserP.curToken.Type {
+	case token.LET:
+		return parserP.ParseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (parserP *Parser) ParseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: parserP.curToken}
+
+	if !parserP.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	for !parserP.curTokenIs(token.SEMICOLON) {
+		parserP.NextToken()
+	}
+
+	return stmt
+}
+
+func (parserP *Parser) curTokenIs(tokenB token.TokenType) bool {
+	return parserP.curToken.Type == tokenB
+}
+func (parserP *Parser) peekTokenIs(tokenB token.TokenType) bool {
+	return parserP.peekToken.Type == tokenB
+}
+func (parserP *Parser) expectPeek(tokenB token.TokenType) bool {
+	if parserP.peekTokenIs(tokenB) {
+		parserP.NextToken()
+		return true
+	} else {
+		return false
+	}
+}
---
+++ parser/parser_test .go
@@ -0,0 +1,64 @@
+package parser
+
+import (
+	"cathon/ast"
+	"cathon/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	let x =5;
+	let y = 10;
+	let foobar = 838383;
+	`
+
+	testLexer := lexer.New(input)
+	testParser := New(testLexer)
+
+	program := testParser.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 Statements. got %d Statements", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !TestLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func TestLetStatement(t *testing.T, testStatement ast.Statement, name string) bool {
+	if testStatement.TokenLiteral() != "let" {
+		t.Errorf("testStatement.TokenLiteral is not 'let'. got %q", testStatement.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := testStatement.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("testStatement is not *ast.LetStatement. got %T", testStatement)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value is not '%s'. got %s", name, letStmt.Name.Value)
+		return false
+	}
+	if testStatement.TokenLiteral() != "let" {
+		t.Errorf("letStmt.Name.TokenLiteral() is not '%s'. got %q", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
+}
